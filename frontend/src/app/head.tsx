591fb24840df187bdce281215d76517cdbfc2a82
luivazmanso7 <luivazmanso7@gmail.com>
2025-07-21 12:30:00 -0300

    head

---
+++ frontend/src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Aces League - Sua Liga de Poker</title>
+      <meta name="description" content="Bem-vindo à Aces League, a plataforma oficial da sua liga de poker." />
+      <meta property="og:title" content="Aces League - Sua Liga de Poker" />
+      <meta property="og:description" content="Bem-vindo à Aces League, a plataforma oficial da sua liga de poker." />
+
+    </>
+  );
+}
